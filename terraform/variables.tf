248a6d00aa45c6e62099e52d84d60a7f12fcb703
sd <ab@cd.com>
2026-06-03 22:35:26 +0545

    update: instance type set to t3.medium

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,4 +2,4 @@ variable "aws_region" { default = "us-east-1" } # This variable is used in provi
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "public_subnet" { default = "10.0.1.0/24" }
 variable "private_subnet" { default = "10.0.2.0/24" }
-variable "instance_type" { default = "t2.micro" }
+variable "instance_type" { default = "t3.medium" }
